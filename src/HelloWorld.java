ffa7aea5c1e6f4ac4587496c88319e2dfc1bd310
HirtenfelderJulian <julian.hirtenfelder@htlpinkafeld.at>
2022-09-16 19:06:50 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+            System.out.println("Hello World!");
+            System.out.println("Welcome to KONF 2022!");
+            System.out.println("Zeile 3");
+            System.out.println("Zeile 4");
+            System.out.println("Zeile 5");
+            System.out.println("Zeile 6"); //Strg + D
+    }
+}
